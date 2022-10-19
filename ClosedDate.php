5fa52909939f3f6575305c8905ebe87d2c84c4b7
priyam759 <59149073+priyam759@users.noreply.github.com>
2022-10-19 15:36:45 -0500

    Add files via upload

---
+++ ClosedDate.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once('core/columns_api.php');
+
+class ClosedDatePluginColumn extends MantisColumn
+{
+	public $title = "Closed Date";
+	public $column = "col";
+	public $sortable = false;
+	public function display( $p_bug, $p_columns_target ){
+		echo db_query("SELECT CASE
+	WHEN bughistory.[field_name] = 'status' AND bughistory.[new_value] = '90' THEN bug_history.[date_modified]
+	ELSE NULL
+	END AS 'date_closed' FROM mantis_bug_history_table bughistory WHERE bughistory.[bug_id]={$p_bug->id}")->fields["date_closed"];
+	}
+
+class ClosedDatePlugin extends MantisPlugin {
+
+	function register() {
+		$this->name = 'Closed Date';
+		$this->description = 'Closed Date plugin will auto-fill the Closed Date custom field with the current timestamp when a ticket is marked as closed';
+		//$this->page = '';
+
+		$this->version = '2.25.1';
+		$this->requires = array(
+			'MantisCore' => '2.0.0'
+		);
+
+		$this->author = 'Priyam Patel';
+		$this->contact = 'priyam.patel@us.glory-global.com';
+		//$this->url = '';
+	}
+	
+	//need to hook event that is called once ticket is closed, and populate custom field value for 'date_closed' once there 
+	
+	function init() {
+		plugin_event_hook( 'EVENT_FILTER_COLUMNS', 'addColumn' );
+	}
+	
+	function addColumn()
+	{
+		return array( 'ClosedDatePluginColumn' );
+	}
+}
+?>
